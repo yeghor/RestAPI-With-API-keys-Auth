e1fa2a7d166306068ff0a510185f8f8931de7b9b
RomanovYehor <yeghor.romanov@gmail.com>
2025-04-05 11:30:30 +0300

    bug fix

--- API_application/jwt_token.py
+++ API_application/jwt_token.py
@@ -6,7 +6,7 @@ def encode_jwt(userinfo: UserSchema):
         "username": userinfo.username,
         "date_joined": userinfo.date_joined,
     }
-    encoded = jwt.encode(payload, userinfo.api_key, algorithm="HS256")
+    return jwt.encode(payload, userinfo.api_key, algorithm="HS256")
 
 def decode_jwt(encoded_jwt, api_key):
     jwt.decode(encoded_jwt, api_key, algorithms=["HS256"])
