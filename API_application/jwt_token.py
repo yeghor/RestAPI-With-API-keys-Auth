5d0c1be4c5fda5552a48c2daefd42093c3e26f71
RomanovYehor <yeghor.romanov@gmail.com>
2025-04-05 11:29:48 +0300

    Added encoding jwt logic

--- API_application/jwt_token.py
+++ API_application/jwt_token.py
@@ -1,4 +1,11 @@
+import jwt
+from schemas import UserSchema
+def encode_jwt(userinfo: UserSchema):
+    payload = {
+        "username": userinfo.username,
+        "date_joined": userinfo.date_joined,
+    }
+    encoded = jwt.encode(payload, userinfo.api_key, algorithm="HS256")
 
-
-def construct_jwt(userinfo: dict):
-    pass
+def decode_jwt(encoded_jwt, api_key):
+    jwt.decode(encoded_jwt, api_key, algorithms=["HS256"])
